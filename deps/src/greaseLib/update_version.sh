6d02767817fe050df7a7f9eb1ae548bce9c2366b
edhemphill <edhemphill@gmail.com>
2018-06-29 13:56:23 -0500

    update update_version.sh

--- deps/src/greaseLib/update_version.sh
+++ deps/src/greaseLib/update_version.sh
@@ -11,6 +11,7 @@ THIS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 pushd $THIS_DIR
 
 COMMIT=`git rev-parse --short=7 HEAD`
+echo "Writing commit number ${COMMIT} to version.h"
 cp version.template.h version.h
 sed -i -e "s/COMMIT_NUMBER/${COMMIT}/g" version.h 
 
