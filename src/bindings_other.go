0b454760a2deb83555e45b63d33c80d4b9ab13f6
edhemphill <edhemphill@gmail.com>
2018-07-23 13:37:05 -0500

    another try to fix pthread build issue

--- src/bindings_other.go
+++ src/bindings_other.go
@@ -6,7 +6,7 @@ package greasego
 
 /*
 #cgo LDFLAGS: -L${SRCDIR}/deps/lib
-#cgo LDFLAGS: -lpthread -lgrease -lstdc++ -lm -ltcmalloc_minimal -lm
+#cgo LDFLAGS: -lgrease -lstdc++ -lm -ltcmalloc_minimal -lm -lpthread -pthread
 #cgo CFLAGS: -I${SRCDIR}/deps/include DEBUG(-DDEBUG_BINDINGS)
 #define GREASE_IS_LOCAL 1
 #include <stdio.h>
